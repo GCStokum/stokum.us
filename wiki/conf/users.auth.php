a9d5a9b1bc3098847d5ae8604cea2b2663bf3250
GCStokum <GCStokum@gmail.com>
2015-01-11 00:25:27 -0800

    intial file push from local

---
+++ wiki/conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# user:MD5password:Real Name:email:groups,comma,seperated
+
+
+admin:$1$shXuza6S$oPmSKGzZ3me.y7NuxO5P10:admin:GCStokum@gmail.com:admin,user
